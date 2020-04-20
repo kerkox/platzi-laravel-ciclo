ff9b4da367e21b2ad0b77da5dccbbd9ce54c9682
Paul Cortes <paulkerg@gmail.com>
2020-04-19 22:36:41 -0500

    Ajuste de formulario de creacion de usuarios

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,23 +1,24 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-    </head>
-    <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-8 mx-auto">
-                <div class="card">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-8 mx-auto">
+            <div class="card border-o shadow">
+                <div class="card-body">
                     <form action="{{ route('users.store') }}" method="POST">
-
                         <div class="form-row">
                             <div class="col-sm-3">
                                 <input type="text" name="name" class="form-control" placeholder="Nombre">
@@ -35,39 +36,40 @@
                         </div>
                     </form>
                 </div>
-
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Nombre</th>
-                            <th>Email</th>
-                            <th>&nbsp</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($users as $user)
-                            <tr>
-                                <td>{{ $user->id }}</td>
-                                <td>{{ $user->name }}</td>
-                                <td>{{ $user->email }}</td>
-                                <td>
-                                    <form action="{{ route('users.destroy', $user) }}" method="POST">
-                                        @method('delete')
-                                        @csrf
-                                        <input
-                                            type="submit"
-                                            value="Eliminar"
-                                            class="btn btn-sm btn-danger"
-                                        onclick="return confirm('¿Desea elminiar...?')">
-                                    </form>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
             </div>
+
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Email</th>
+                    <th>&nbsp</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($users as $user)
+                    <tr>
+                        <td>{{ $user->id }}</td>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>
+                            <form action="{{ route('users.destroy', $user) }}" method="POST">
+                                @method('delete')
+                                @csrf
+                                <input
+                                    type="submit"
+                                    value="Eliminar"
+                                    class="btn btn-sm btn-danger"
+                                    onclick="return confirm('¿Desea elminiar...?')">
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
-    </body>
+</div>
+</body>
 </html>
