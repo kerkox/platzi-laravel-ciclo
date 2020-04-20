d664d7b0ef6e91fc6c41d7f22f648ccff5033e9b
Paul Cortes <paulkerg@gmail.com>
2020-04-19 22:47:16 -0500

    listado de errores de formulario de creacion, y restauracion de valores para que no se pierdan

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -17,14 +17,21 @@
     <div class="row">
         <div class="col-sm-8 mx-auto">
             <div class="card border-o shadow">
+                @if($errors->any())
+                    <div class="alert alert-danger">
+                        @foreach($errors->all() as $error)
+                            - {{ $error }}<br>
+                        @endforeach
+                    </div>
+                @endif
                 <div class="card-body">
                     <form action="{{ route('users.store') }}" method="POST">
                         <div class="form-row">
                             <div class="col-sm-3">
-                                <input type="text" name="name" class="form-control" placeholder="Nombre">
+                                <input type="text" name="name" class="form-control" placeholder="Nombre" value="{{ old('name') }}">
                             </div>
                             <div class="col-sm-4">
-                                <input type="email" name="email" class="form-control" placeholder="Email">
+                                <input type="email" name="email" class="form-control" placeholder="Email" value="{{ old('email') }}">
                             </div>
                             <div class="col-sm-3">
                                 <input type="password" name="password" class="form-control" placeholder="Contraseña">
