4777220395c345d78620d7206d44abb0fedae494
Paul Cortes <paulkerg@gmail.com>
2020-04-19 22:02:51 -0500

    se agrega vista y logica a controlador User

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::latest()->get();
+        return view('users.index', [
+            'users' => $users
+        ]);
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,5 @@
 */
 
 Route::get('/', 'UserController@index');
+Route::post('/users', 'UserController@store')->name('users.store');
+Route::delete('/users/{user}', 'UserController@destroy')->name('users.destroy');
